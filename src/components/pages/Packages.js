75bdcd1ce8cb311d0940eb5384c9aaae272ad936
jamesbull24 <jamesbull@live.com>
2021-02-09 22:22:00 +0000

    SEO adjustments

--- src/components/pages/Packages.js
+++ src/components/pages/Packages.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import '../../App.js';
 
-export default function Packages() {
-    return <h1 className="packages">Our Packages</h1>;
+function Packages() {
+    return (
+        <h1 className="packages">Our Packages</h1>
+    )
 }
+
+export default Packages;
