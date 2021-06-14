b7813715df4877710f25bff66a5cfd90fc4148f9
James <jamesbull@live.com>
2021-06-14 10:55:57 +0100

    update packages

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -54,7 +54,6 @@ export default class extends React.Component {
 			.then((res) => {
 				console.log('Email successfully sent!');
 			})
-			// Handle errors here however you like, or use a React error boundary
 			.catch((err) =>
 				console.error(
 					'FAILED. Here some thoughts on the error that occured:',
